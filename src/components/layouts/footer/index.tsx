351e8af68d685cd1def503e8c3df74983c467b95
eri-zu <izutsu812@gmail.com>
2025-07-26 00:52:01 +0900

    fix: animation

--- src/components/layouts/footer/index.tsx
+++ src/components/layouts/footer/index.tsx
@@ -1,10 +1,27 @@
+"use client";
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+
 import style from "./index.module.scss";
 
 import type React from "react";
 
 export const Footer: React.FC = () => {
+  const footerRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(() => {
+    gsap.to(footerRef.current, {
+      opacity: 1,
+      duration: 1.0,
+      ease: "power2.inOut",
+      delay: 0.1 * 4,
+    });
+  });
+
   return (
-    <footer className={style.footer}>
+    <footer className={style.footer} ref={footerRef} style={{ opacity: 0.001 }}>
       <div className={style.footerInner}>
         <p className={style.footerText}>
           <small>&copy; {new Date().getFullYear()} ERI IZUTSU</small>
--- src/components/ui/animationControlButton/index.tsx
+++ src/components/ui/animationControlButton/index.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+
 import style from "./index.module.scss";
 import { useAnimationStore } from "../../../stores/animationStore";
 
@@ -8,13 +12,29 @@ import type React from "react";
 export const AnimationControlButton: React.FC = () => {
   const isAnimation = useAnimationStore((s) => s.isAnimation);
   const setIsAnimation = useAnimationStore((s) => s.setIsAnimation);
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
   const onClick = () => {
     setIsAnimation(!isAnimation);
   };
 
+  useGSAP(() => {
+    gsap.to(buttonRef.current, {
+      opacity: 1,
+      duration: 1.0,
+      ease: "power2.inOut",
+      delay: 0.1 * 4,
+    });
+  });
+
   return (
-    <button type="button" className={style.button} onClick={onClick}>
+    <button
+      type="button"
+      className={style.button}
+      onClick={onClick}
+      ref={buttonRef}
+      style={{ opacity: 0.001 }}
+    >
       <svg
         viewBox="0 0 50 50"
         fill="none"
